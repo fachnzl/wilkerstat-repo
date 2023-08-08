9b40d9afd2a630230b71289be2d3087f391fb160
fachnzl <141256779+fachnzl@users.noreply.github.com>
2023-08-08 10:38:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ with open("css/style.css") as source_des:
         int)
     wilkerstat_subsls['jumlah_art_tani'] = wilkerstat_subsls['jumlah_art_tani'].astype(
         int)
-
+    coltab = st.columns(1)
     pml = st.multiselect('Which beverage do you like?', ['Tea', 'Coffee','Iced Tea' ,'Diet Coke', 'Lemonade'],['Tea', 'Coffee'])
     st.markdown(pml)
-    st.table(repo_subsls.merge(wilkerstat_subsls, how="left", on='idsubsls'))
+    coltab.table(repo_subsls.merge(wilkerstat_subsls, how="left", on='idsubsls'))
 # except:
 #     st.markdown("Data Belum di update")
